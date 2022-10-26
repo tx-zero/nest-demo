06cddce74e6ad52d11b3d129832a5d050cbc3e0c
jharrisonio <james@jharrison.io>
2022-10-26 15:52:03 -0700

    Forcing port 80

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT ?? 80;
+  const port = 80;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
